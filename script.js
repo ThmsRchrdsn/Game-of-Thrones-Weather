ef7c431f9982c87c45fd0733b9e5a4eddf593b72
ThmsRchrdsn <115042486+ThmsRchrdsn@users.nereply.github.com>
2022-11-29 09:33:18 +0000

    Added The Wall image and changed the compared city getter for better use, all citys in an array

--- script.js
+++ script.js
@@ -14,11 +14,27 @@ const temperatureDescriptorArray = [
   "Scorching Hot",
 ];
 const backgroundImages = [
+  'url("./gallery/The Wall.jpg")',
   'url("./gallery/Winterfell.jpg")',
   'url("./gallery/Casterly Rock.jpg")',
   'url("./gallery/Kings Landing.jpg")',
-  'url("./gallery/Dorne.jpg")',
+  'url("./gallery/Sunspear.jpg")',
   'url("./gallery/Qarth.jpg")',
+];
+const comparedCities = [
+  "The Wall",
+  "Winterfell",
+  "Casterly Rock",
+  "Kings Landing",
+  "Sunspear",
+  "Qarth",
+];
+const comparedCitiesQuotes = [
+  '"The Lord of Winterfell would always be a Stark. —thoughts of Tyrion Lannister"',
+  '"Stone, I must be stone, I must be Casterly Rock, hard and unmovable." —thoughts of Tyrion Lannister',
+  '"Kings Landing is a pit of snakes." —Doran Martell to Nymeria Sand',
+  '"Dorne is sand and scorpions, and bleak red mountains baking in the sun." —Reznak mo Reznak to Skahaz mo Kandaq',
+  '"These are strange times in Qarth. And strange times are bad for trade." —Xaro Xhoan Daxos to Daenerys Targaryen',
 ];
 
 check.addEventListener("click", () => {
@@ -30,7 +46,7 @@ check.addEventListener("click", () => {
       return response.json();
     })
     .then((data) => {
-      //   console.log(data);
+      // console.log(data.clouds.all);
       weatherCountry.innerText = `${data.name} / ${data.sys.country}`;
       getCityFromDescription(data.main.temp);
       weatherDescription.innerText =
@@ -44,40 +60,23 @@ check.addEventListener("click", () => {
 });
 
 function getCityFromDescription(temperature) {
-  if (temperature <= 0) {
-    temperatureDescription = temperatureDescriptorArray[0];
-    document.getElementById("container").style.backgroundImage =
-      backgroundImages[0];
-    fictionalComparedCity.innerText = "Winterfell";
-    fictionalCityQuote.innerText = '"The Lord of Winterfell would always be a Stark. —thoughts of Tyrion Lannister"';
-
+  let cityNumber;
+  if (temperature <= -10) {
+    cityNumber = 0;
+  } else if (temperature > -10 && temperature <= 0) {
+    cityNumber = 1;
   } else if (temperature > 0 && temperature <= 10) {
-    temperatureDescription = temperatureDescriptorArray[1];
-    console.log(backgroundImages[1]);
-    document.getElementById("container").style.backgroundImage =
-      backgroundImages[1];
-    fictionalComparedCity.innerText = "Casterly Rock";
-    fictionalCityQuote.innerText = '"Stone, I must be stone, I must be Casterly Rock, hard and unmovable." —thoughts of Tyrion Lannister';
-
+    cityNumber = 2;
   } else if (temperature > 10 && temperature <= 20) {
-    temperatureDescription = temperatureDescriptorArray[2];
-    document.getElementById("container").style.backgroundImage =
-      backgroundImages[2];
-    fictionalComparedCity.innerText = 'King'+"'"+'s Landing';
-    fictionalCityQuote.innerText = '"King'+"'"+'s Landing is a pit of snakes." —Doran Martell to Nymeria Sand';
-
+    cityNumber = 3;
   } else if (temperature > 20 && temperature <= 30) {
-    temperatureDescription = temperatureDescriptorArray[3];
-    document.getElementById("container").style.backgroundImage =
-      backgroundImages[3];
-    fictionalComparedCity.innerText = "Dorne";
-    fictionalCityQuote.innerText = '"Dorne is sand and scorpions, and bleak red mountains baking in the sun." —Reznak mo Reznak to Skahaz mo Kandaq';
-
-  } else {
-    temperatureDescription = temperatureDescriptorArray[4];
-    document.getElementById("container").style.backgroundImage =
-      backgroundImages[4];
-    fictionalComparedCity.innerText = "Qarth";
-    fictionalCityQuote.innerText = '"These are strange times in Qarth. And strange times are bad for trade." —Xaro Xhoan Daxos to Daenerys Targaryen';
+    cityNumber = 4;
+  } else if (temperature > 30) {
+    cityNumber = 5;
   }
+  temperatureDescription = temperatureDescriptorArray[cityNumber];
+  document.getElementById("container").style.backgroundImage =
+    backgroundImages[cityNumber];
+  fictionalComparedCity.innerText = comparedCities[cityNumber];
+  // fictionalCityQuote.innerText = comparedCitiesQuotes[cityNumber];
 }
